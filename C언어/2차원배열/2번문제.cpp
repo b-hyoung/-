103047656f70cc2cebbfd195a0330b9f09cc126a
b-hyoung <youqlrqod@gmail.com>
2025-05-08 16:46:22 +0900

    real finally

--- C언어/2차원배열/2번문제.cpp
+++ C언어/2차원배열/2번문제.cpp
@@ -4,31 +4,38 @@
 int main() {
     int k,e,m,s;
     int i,j;
+    int sum=0;
     int data[100][100]={0};
-    printf("4과목 성적 입력 (국어, 영어, 수학 , 국사) : \n");
     for(i=0;i<4;i++){
+        printf("4과목 성적 입력 (국어, 영어, 수학 , 국사) :");
         scanf("%d %d %d %d",&k,&e,&m,&s);
         for(j=0;j<4;j++){
             switch(j){   
                 case 0:
                     data[i][j]=k;
+                    data[4][j]+=k;
+                    break;
                 case 1:
                     data[i][j]=e;
+                    data[4][j]+=e;
+                    break;
                 case 2:
                     data[i][j]=m;
+                    data[4][j]+=m;
+                    break;
                 case 3:
                     data[i][j]=s;
+                    data[4][j]+=s;
+                    break;
             }
-        if(j==3){
-            data[i][j+1]=k+e+m+s;
-        }
-        data[4][i]+=data[i][0];
+        data[i][j+1]=k+e+m+s;
     }    
+        data[4][4]+=data[i][4];
     }
-
+    printf("\n국어 영어 수학 국사 총점\n");
     for(i=0;i<5;i++){
         for(j=0;j<5;j++){
-            printf("%3d",data[i][j]);
+            printf("%4d",data[i][j]);
         }
         printf("\n");
     }
--- C언어/2차원배열/과제.cpp
+++ C언어/2차원배열/과제.cpp
@@ -1,20 +1,34 @@
 #include <stdio.h>
 
-int main() {
-    int i,j;
-    int row=9,col=3;
-    int data[100][100]={0};
-    int start=2,end=4;
-    for(i=start;i<=end;i++){
-        for(j=1;j<=row;j++){
-            data[i-start][j-1]=i*j;
+int main(){
+	
+	int i,j;
+	int data[100][100]={0};
+	int input;
+    scanf("%d",&input);
+    int k=1;
+    int sta=input;
+    int end=input;
+	// 4,4 4,0 3,1 2 3
+	for(i=0;i<input;i++){
+        for(j=sta;j<=end;j++){
+            data[j][i]=k++;
         }
+        sta--;
+        end++;
     }
-    for(i=0;i<col;i++){
-        for(j=0;j<row;j++){
-            printf("%3d",data[i][j]);
+
+    
+    for(i=0;i<=input*2-1;i++){
+        for(j=0;j<=input;j++){
+            if(data[i][j] == 0){
+                printf("%3c" ,' ');
+            }else{
+                printf("%3d" , data[i][j]);
+            }
         }
         printf("\n");
     }
-    return 0;
+	
+	return 0;
 }
