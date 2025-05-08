948b131df35bb22b449105385a17e148851decf7
b-hyoung <youqlrqod@gmail.com>
2025-05-08 13:47:46 +0900

    add gugu

--- C언어/2차원배열/구구단저장p1.cpp
+++ C언어/2차원배열/구구단저장p1.cpp
@@ -1,20 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+    int k,e,m,s;
     int i,j;
-    int row=9,col=3;
+    int sum=0;
     int data[100][100]={0};
-    int start=2,end=4;
-    for(i=start;i<=end;i++){
-        for(j=1;j<=row;j++){
-            data[i-start][j-1]=i*j;
-        }
+    for(i=0;i<4;i++){
+        printf("4과목 성적 입력 (국어, 영어, 수학 , 국사) :");
+        scanf("%d %d %d %d",&k,&e,&m,&s);
+        for(j=0;j<4;j++){
+            switch(j){   
+                case 0:
+                    data[i][j]=k;
+                    data[4][j]+=k;
+                    break;
+                case 1:
+                    data[i][j]=e;
+                    data[4][j]+=e;
+                    break;
+                case 2:
+                    data[i][j]=m;
+                    data[4][j]+=m;
+                    break;
+                case 3:
+                    data[i][j]=s;
+                    data[4][j]+=s;
+                    break;
+            }
+        data[i][j+1]=k+e+m+s;
+    }    
+        data[4][4]+=data[i][4];
     }
-    for(i=0;i<col;i++){
-        for(j=0;j<row;j++){
-            printf("%3d",data[i][j]);
+    printf("\n국어 영어 수학 국사 총점\n");
+    for(i=0;i<5;i++){
+        for(j=0;j<5;j++){
+            printf("%4d",data[i][j]);
         }
         printf("\n");
     }
+    
     return 0;
 }
